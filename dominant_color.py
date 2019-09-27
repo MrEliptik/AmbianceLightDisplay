5f47525aa9df35611e926387b4323da3af17d313
Victor Meunier <mreliptik@pop-os.localdomain>
2019-09-27 19:47:39 +0200

    add timing debug level

--- dominant_color.py
+++ dominant_color.py
@@ -25,29 +25,34 @@ def getDominantColor(image, num_clusters=3):
 
 def worker(input_q, output_q):
     while True:
-        #print("> ===== in worker loop, frame ", frame_processed)
         patch_id, frame = input_q.get()
         if (frame is not None):
             color = getDominantColor(frame)
             output_q.put((patch_id, color))
 
-def main(display=False, debug=False):
+def grabber(output_q):
+    while True:
+        # Get raw pixels from the screen, save it to a Numpy array
+        im = np.array(sct.grab(sct.monitors[monitor['id']])) # ~35ms on 4k display, 20ms 1080p
+
+def main(display=False, debug="none"):
+    debug = False
+    timing = False
+    if debug == "debug":
+        debug = True
+        timing = True
+    elif debug == "timing":
+        timing = True
+
+
     ## FLAGS
     # Full speed if -1
-    FIXED_FPS = 10
-
-    COL_SCAN = 8
-    ROW_SCAN = 4
-
-    PATCHES_NB = ((COL_SCAN+ROW_SCAN)*2) - 4
-
+    FIXED_FPS = 20
     NB_MONITOR = 1
-    MONITOR_1 = {'id':1, 'w':2560, 'h':1080, 'active':False}
-    MONITOR_2 = {'id':2, 'w':3840, 'h':2160, 'active':True}
+    MONITOR_1 = {'id':1, 'w':2560, 'h':1080, 'col':6, 'row':4,'active':True}
+    MONITOR_2 = {'id':2, 'w':3840, 'h':2160, 'col':8, 'row':4, 'active':False}
     MONITORS = [MONITOR_1, MONITOR_2]
 
-    patches = [None]*PATCHES_NB
-    colors = [None]*PATCHES_NB
 
     if debug:
         print("Creating queues..")
@@ -57,8 +62,8 @@ def main(display=False, debug=False):
     if debug:
         print('Spinning up workers..')
     # Spin up workers to parallelize workload
-    pool = Pool(16, worker, (input_q, output_q))
-
+    pool = Pool(8, worker, (input_q, output_q))
+    #grabber_pool = Pool(1, grabber, (input_q, output_q))
     
     # Take screenshot
     with mss.mss() as sct:
@@ -68,15 +73,20 @@ def main(display=False, debug=False):
                 print('Starts grabbing..')
             last_time = time.time()
 
-            if debug:
+            if timing:
                 last_screen = time.time()
 
             for monitor in MONITORS:
                 if not monitor['active']:
                     continue
+
+                PATCHES_NB = ((monitor['col']+monitor['row'])*2) - 4
+                patches = [None]*PATCHES_NB
+                colors = [None]*PATCHES_NB
+                
                 # Get raw pixels from the screen, save it to a Numpy array
                 im = np.array(sct.grab(sct.monitors[monitor['id']])) # ~35s on 4k display, 20ms 1080p
-                if debug:
+                if timing:
                     print("Screen time: {} ms".format((time.time() - last_screen)*1000))
 
                 last_time_delay = time.time()
@@ -86,45 +96,56 @@ def main(display=False, debug=False):
                 im = cv.resize(im, (int(w/6), int(h/6)), cv.INTER_LINEAR)
 
                 h, w, _ = im.shape
+                if debug:
+                    print('h: {}, w: {}'.format(h, w))
 
-                kernel_col_size = h//ROW_SCAN
-                kernel_row_size = w//COL_SCAN
+                kernel_col_size = h//monitor['row']
+                kernel_row_size = w//monitor['col']
 
+                if timing:
+                    calc_time = time.time()
+                    patch_extract_time = time.time()
                 if debug:
                     print('kernel size: {},{}'.format(kernel_col_size, kernel_row_size))
-
-                # size is == to rectangle's perimeter
-                
-                if debug:
                     print('patch size: {}'.format(PATCHES_NB))
                     print('Starts processing screenshot')
-                u = 0 
-                l = 0 
-                for i in range(ROW_SCAN):
-                    for j in range(COL_SCAN):
+                    l = 0
+                _id = 0 
+                for i in range(monitor['row']):
+                    for j in range(monitor['col']):
                         x1 = (j*kernel_row_size)
                         x2 = (kernel_row_size+x1)
                         y1 = (i*kernel_col_size) 
                         y2 = (kernel_col_size+y1) 
 
-                        l += 1
                         if debug:
                             print('Patches: {}'.format(l))
                             print('Coords..: {}, {}, {}, {}'.format(x1,y1,x2,y2))
+                            l += 1
                         if x1 == 0 or y1 == 0 or x2 == w or y2 == h:
-                            input_q.put((u, im[y1:y2, x1:x2, :]))
-                            u += 1
+                            input_q.put((_id, im[y1:y2, x1:x2, :]))
+                            _id += 1
                             if debug:
                                 print('Input q full: {}'.format(input_q.empty()))
-                                print('Input q size: {}'.format(u))
-                            
+                                print('Input q size: {}'.format(_id))
+                if timing:
+                    print('Patch extraction: {} ms'.format((time.time() - patch_extract_time)*1000))
+
+                if timing:
+                    last_process_finished = time.time()   
                 k = 0
                 while(k < PATCHES_NB): 
                     if debug:
                         print('Waiting for patches {}'.format(k))
                     patch_id, patch = output_q.get()
+                    if timing:
+                        print('Process {} finished: {} ms'.format(patch_id, (time.time() - last_process_finished)*1000))
                     colors[patch_id] = patch
                     k += 1
+                
+                if timing:
+                    print('Last process finished: {} ms'.format((time.time() - last_process_finished)*1000))
+                    print('calc time: {} ms'.format((time.time() - calc_time)*1000))
 
                 if debug:
                     print(colors)
@@ -158,4 +179,4 @@ def main(display=False, debug=False):
                     cv.imshow("OpenCV/Numpy normal", im)
 
 if __name__ == "__main__":
-    main(display=True)
+    main(display=True, debug="none")
